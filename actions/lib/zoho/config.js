0d695532a2214e2fbb138cc474bc053e7a4f225c
touta <marvette.feghali@elementn.com>
2017-09-22 13:15:19 +0300

    latest updates

---
+++ actions/lib/zoho/config.js
@@ -0,0 +1,4 @@
+var crmAuthToken = "";
+var supportAuthToken ="921083796e08653ed10b28e8f8ea35cd"; //da33afb40050390eb35fed5c8a0d9b9e | 921083796e08653ed10b28e8f8ea35cd
+var portal = "scritpr";
+var department = "scritpr";
